525f6315c54786e77bbf67e4bcd293e4215c8993
Brieuc Caillot <brieucdeveloper@gmail.com>
2021-02-11 14:42:22 +0100

    Removed css & renamed to App.vue

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <h1>Safe Place</h1>
+    <router-view v-slot="{ Component }">
+      <Suspense>
+        <component :is="Component" />
+      </Suspense>
+    </router-view>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: 'Safe Place',
+    }
+  },
+}
+</script>
