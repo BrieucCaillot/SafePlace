df369f57926408b09acb4691e376d75b973222d2
LeonBaudouin <43001396+LeonBaudouin@users.noreply.github.com>
2021-02-12 15:48:28 +0100

    Reorganised scene and handle resize

--- src/three/Scene.ts
+++ src/three/Scene.ts
@@ -3,6 +3,7 @@ import ThreeObject from './Abstract/ThreeObject'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 export default class Scene {
+  public canvas: HTMLCanvasElement
   public controls: OrbitControls
   public scene: THREE.Scene
   public renderer: THREE.WebGLRenderer
@@ -11,28 +12,12 @@ export default class Scene {
   public sizes = { width: 0, height: 0 }
 
   constructor(canvas: HTMLCanvasElement) {
-    // Set window sizes
-    this.sizes.width = window.innerWidth
-    this.sizes.height = window.innerHeight
-
+    this.canvas = canvas
     this.scene = new THREE.Scene()
 
-    this.renderer = new THREE.WebGLRenderer({
-      canvas,
-      alpha: true,
-      antialias: true,
-    })
-    this.renderer.setSize(this.sizes.width, this.sizes.height)
-    this.renderer.setPixelRatio(window.devicePixelRatio)
-
-    this.renderer.setClearColor(0x000000)
-
-    this.camera = new THREE.PerspectiveCamera(50, this.sizes.width / this.sizes.height, 0.1, 1000)
-    this.camera.position.z = 5
-
-    this.controls = new OrbitControls(this.camera, canvas)
-
-    this.scene.add(this.camera)
+    this.SetupSize()
+    this.SetupRenderer()
+    this.SetupCamera()
   }
 
   public Update() {
@@ -65,9 +50,40 @@ export default class Scene {
 
   public Delete() {
     this.CleanObjects()
+    window.removeEventListener('resize', this.OnResize)
   }
 
-  public OnResize() {
+  private SetupRenderer() {
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: this.canvas,
+      alpha: true,
+      antialias: true,
+    })
+    this.renderer.setSize(this.sizes.width, this.sizes.height)
+    this.renderer.setPixelRatio(window.devicePixelRatio)
+
+    this.renderer.setClearColor(0x000000)
+  }
+
+  private SetupCamera() {
+    this.camera = new THREE.PerspectiveCamera(50, this.sizes.width / this.sizes.height, 0.1, 1000)
+    this.camera.position.z = 5
+
+    this.controls = new OrbitControls(this.camera, this.canvas)
+
+    this.scene.add(this.camera)
+  }
+
+  private SetupSize() {
+    // Set window sizes
+    this.sizes.width = window.innerWidth
+    this.sizes.height = window.innerHeight
+
+    this.OnResize = this.OnResize.bind(this)
+    window.addEventListener('resize', this.OnResize)
+  }
+
+  private OnResize() {
     // Update sizes
     this.sizes.width = window.innerWidth
     this.sizes.height = window.innerHeight
