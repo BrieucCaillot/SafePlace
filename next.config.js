0ff6130121de7f1ca62d09e413f571838ef5c8a7
LeonBaudouin <43001396+LeonBaudouin@users.noreply.github.com>
2021-03-03 11:36:59 +0100

    Enabled side effect on react spring

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+// https://github.com/pmndrs/drei/issues/2
 const withTM = require('next-transpile-modules')([
   'three',
   '@react-three/drei',
@@ -7,11 +8,18 @@ const withTM = require('next-transpile-modules')([
 module.exports = withTM({
   productionBrowserSourceMaps: true,
   webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.(glsl|vs|fs|vert|frag)$/,
-      exclude: /node_modules/,
-      use: ['raw-loader', 'glslify-loader'],
-    })
+    config.module.rules.push(
+      {
+        test: /\.(glsl|vs|fs|vert|frag)$/,
+        exclude: /node_modules/,
+        use: ['raw-loader', 'glslify-loader'],
+      },
+      // https://github.com/pmndrs/react-spring/issues/1078
+      {
+        test: /react-spring/,
+        sideEffects: true,
+      }
+    )
 
     return config
   },
