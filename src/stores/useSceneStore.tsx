ababf59410de47b5eb9e67baeaed535e9ba70882
Brieuc Caillot <brieucdeveloper@gmail.com>
2021-04-07 18:03:58 +0200

    Added useMountainJourneyStore & useSceneStore

---
+++ src/stores/useSceneStore.tsx
@@ -0,0 +1,27 @@
+import { get } from 'node:http'
+import * as THREE from 'three'
+import create from 'zustand'
+
+export enum Scenes {
+  Safeplace = 'Safeplace',
+  About = 'About',
+  MountainJourney = 'MountainJourney',
+}
+
+type SceneStore = {
+  currentScene: Scenes
+  setCurrentScene: (scene: Scenes) => void
+  isCurrentScene: (scene) => boolean
+  showSafeplace: boolean
+  setShowSafeplace: (status: boolean) => void
+}
+
+const useSceneStore = create<SceneStore>((set, get) => ({
+  currentScene: Scenes.Safeplace,
+  setCurrentScene: (scene) => set({ currentScene: scene }),
+  isCurrentScene: (scene) => scene == get().currentScene,
+  showSafeplace: true,
+  setShowSafeplace: (status) => set({ showSafeplace: status }),
+}))
+
+export default useSceneStore
