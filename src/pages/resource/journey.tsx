46fb1b9f4155e10265598f04c432cd70535cdc94
Brieuc Caillot <brieucdeveloper@gmail.com>
2021-05-03 23:33:27 +0200

    Quickfix to prevent journey ambiant to continue when back in the safeplace

--- src/pages/resource/journey.tsx
+++ src/pages/resource/journey.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import ResourceContent from '@/components/Safeplace/UI/ResourceContent'
 import PortalUI from '@/components/common/UI/PortalUI'
+import SafeplaceAudio from '@/components/Safeplace/SafeplaceAudio'
 import ResourcesButton from '@/components/Safeplace/UI/ResourcesButton'
 
 const ResourceJourney = () => {
   return (
     <>
+      <SafeplaceAudio />
       <ResourceContent />
       <PortalUI selector='#layout-bottom-left'>
         <ResourcesButton />
