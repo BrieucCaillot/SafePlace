be74517e86fefbd13f7481027b277155a51841ec
Brieuc Caillot <brieucdeveloper@gmail.com>
2021-05-15 20:11:50 +0200

    Pass transition status props to page

---
+++ src/hooks/useTransitionStatus.ts
@@ -0,0 +1,22 @@
+import { useEffect, useRef, useState } from 'react'
+import { TransitionStatus } from 'react-transition-group'
+
+const useTransitionStatus = (status: TransitionStatus) => {
+  const prevStatus = useRef<TransitionStatus>()
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    if (status === prevStatus.current) return
+    prevStatus.current = status
+    if (['entering', 'entered'].includes(status)) {
+      setShow(true)
+    }
+    if (['exiting', 'exited'].includes(status)) {
+      setShow(false)
+    }
+  }, [status])
+
+  return show
+}
+
+export default useTransitionStatus
