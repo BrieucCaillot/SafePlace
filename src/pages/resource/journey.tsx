702a6c9c85008b79f24b4885b514e7908600423b
Brieuc Caillot <brieucdeveloper@gmail.com>
2021-05-23 15:01:21 +0200

    Fix ResourceContent's css transition

--- src/pages/resource/journey.tsx
+++ src/pages/resource/journey.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { TransitionStatus } from 'react-transition-group'
+
+import useTransitionStatus from '@/hooks/useTransitionStatus'
 
 import ResourceContent from '@/components/Safeplace/UI/ResourceContent'
 
-const ResourceJourney = () => {
+const ResourceJourney = ({ status }: { status: TransitionStatus }) => {
+  const show = useTransitionStatus(status)
+
   return (
     <>
       <div className='flex flex-col h-full justify-center'>
-        <ResourceContent />
+        <ResourceContent show={show} />
       </div>
     </>
   )
