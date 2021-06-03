d7c9ce97dd1f47bc3688449cc07ed52a87fcf633
LeonBaudouin <43001396+LeonBaudouin@users.noreply.github.com>
2021-06-03 22:10:07 +0200

    Inverted displacement

--- src/components/Journey/Canvas/Scenes/Intro/Mountain/MountainPlane.fs
+++ src/components/Journey/Canvas/Scenes/Intro/Mountain/MountainPlane.fs
@@ -9,7 +9,7 @@ varying vec2 vUv;
 
 void main() {
   float depth = texture2D(uDepthMap, vUv).r;
-  vec4 textureColor = texture2D(uTexture, vUv + uMousePos * uDisplacementAmount * depth);
+  vec4 textureColor = texture2D(uTexture, vUv + uMousePos * uDisplacementAmount * (1. - depth));
 
   gl_FragColor = vec4(textureColor);
 }
