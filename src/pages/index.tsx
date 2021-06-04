20b9c30132b35cdfaef1b7259ff981e22710f560
Brieuc Caillot <brieucdeveloper@gmail.com>
2021-06-04 17:16:08 +0200

    Removed duplicated CSSTransition & improved transition when going to onboarding page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
 import { CSSTransition, TransitionStatus } from 'react-transition-group'
 
 import useUserStore from '@/stores/useUserStore'
@@ -29,17 +27,14 @@ const Index = ({ status }: { status: TransitionStatus }) => {
     if (previousPathname === Routes.About) setShowSafeplaceAnim(true)
   }, [previousPathname])
 
-  const onAboutButtonClick = useCallback(() => {
-    setShowSafeplaceAnim(true)
-    setHide(true)
-    setTimeout(() => router.push(Routes.About), 2000)
-  }, [router])
-
-  const onStartButtonClick = useCallback(() => {
-    setShowSafeplaceAnim(false)
-    setHide(true)
-    setTimeout(() => router.push(Routes.OnBoarding), 1500)
-  }, [router])
+  const onButtonClicked = useCallback(
+    (route: Routes) => {
+      setShowSafeplaceAnim(true)
+      setHide(true)
+      setTimeout(() => router.push(route), 2000)
+    },
+    [router]
+  )
 
   return (
     <>
@@ -47,7 +42,7 @@ const Index = ({ status }: { status: TransitionStatus }) => {
         <ButtonStonecut
           show={show && !hide}
           className='text-primary cursor-pointer pointer-events-none opacity-0'
-          onClick={onAboutButtonClick}
+          onClick={() => onButtonClicked(Routes.About)}
         >
           A propos
         </ButtonStonecut>
@@ -63,31 +58,25 @@ const Index = ({ status }: { status: TransitionStatus }) => {
           unmountOnExit
           appear
         >
-          <img
-            className='absolute top-1/2 left-1/2 transform-gpu -translate-x-1/2 -translate-y-1/2 -mt-28 max-w-xs md:max-w-full'
-            src='/img/index/logo.png'
-            alt='safeplace-logo'
-          />
-        </CSSTransition>
-        <CSSTransition
-          in={show && !hide}
-          timeout={2000}
-          classNames='elem-fade'
-          mountOnEnter
-          unmountOnExit
-          appear
-        >
-          <div
-            className='absolute bottom-16 flex justify-center w-full'
-            style={{ perspective: '2500px' }}
-          >
-            <button
-              id='button-start'
-              onClick={onStartButtonClick}
-              className='relative button-stonefull text-white text-xl font-bold tracking-widest rounded px-10 py-3 focus:outline-none cursor-pointer'
+          <div>
+            <img
+              className='absolute top-1/2 left-1/2 transform-gpu -translate-x-1/2 -translate-y-1/2 -mt-28 max-w-xs md:max-w-full'
+              src='/img/index/logo.png'
+              alt='safeplace-logo'
+            />
+
+            <div
+              className='absolute bottom-16 flex justify-center w-full pointer-events-auto'
+              style={{ perspective: '2500px' }}
             >
-              Lancer l'experience
-            </button>
+              <button
+                id='button-start'
+                onClick={() => onButtonClicked(Routes.OnBoarding)}
+                className='relative button-stonefull text-white text-xl font-bold tracking-widest rounded px-10 py-3 focus:outline-none cursor-pointer'
+              >
+                Lancer l'experience
+              </button>
+            </div>
           </div>
         </CSSTransition>
       </div>
