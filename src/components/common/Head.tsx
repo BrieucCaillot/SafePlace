dee4d7d7f48070a43489549a207549db9ec630c6
Brieuc Caillot <brieucdeveloper@gmail.com>
2021-06-07 11:38:23 +0200

    Added favicon

--- src/components/common/Head.tsx
+++ src/components/common/Head.tsx
@@ -41,33 +41,77 @@ const Head = () => {
           href='https://fonts.googleapis.com/css2?family=Philosopher:ital,wght@0,400;0,700;1,400&display=swap'
           rel='stylesheet'
         />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Tenor+Sans&display=swap'
-          rel='stylesheet'
-        />
-        <link rel='apple-touch-icon' href='/icons/apple-touch-icon.png' />
+
         <link
           rel='apple-touch-icon'
-          sizes='16x16'
-          href='/icons/favicon-16x16.png'
+          sizes='57x57'
+          href='/img/favicons/apple-icon-57x57.png'
         />
         <link
           rel='apple-touch-icon'
-          sizes='32x32'
-          href='/icons/favicon-32x32.png'
+          sizes='60x60'
+          href='/img/favicons/apple-icon-60x60.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='72x72'
+          href='/img/favicons/apple-icon-72x72.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='76x76'
+          href='/img/favicons/apple-icon-76x76.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='114x114'
+          href='/img/favicons/apple-icon-114x114.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='120x120'
+          href='/img/favicons/apple-icon-120x120.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='144x144'
+          href='/img/favicons/apple-icon-144x144.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='152x152'
+          href='/img/favicons/apple-icon-152x152.png'
         />
         <link
           rel='apple-touch-icon'
           sizes='180x180'
-          href='/icons/apple-touch-icon.png'
+          href='/img/favicons/apple-icon-180x180.png'
         />
-        <link rel='manifest' href='/manifest.json' />
         <link
-          rel='mask-icon'
-          color='#000000'
-          href='/icons/safari-pinned-tab.svg'
+          rel='icon'
+          type='image/png'
+          sizes='192x192'
+          href='/img/favicons/android-icon-192x192.png'
         />
-        <link rel='apple-touch-startup-image' href='/startup.png' />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/img/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='96x96'
+          href='/img/favicon-96x96.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/img/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/img/manifest.json' />
 
         {/* Meta Tags for HTML pages on Mobile */}
         {/* <meta name="format-detection" content="telephone=yes"/>
@@ -77,7 +121,9 @@ const Head = () => {
           content='width=device-width, minimum-scale=1, initial-scale=1.0'
         />
         <meta name='theme-color' content='#000' />
-        <link rel='shortcut icon' href='/icons/favicon.ico' />
+        <meta name='msapplication-TileColor' content='#ffffff' />
+        <meta name='msapplication-TileImage' content='/ms-icon-144x144.png' />
+        <meta name='theme-color' content='#ffffff'></meta>
 
         {/*
       Twitter Summary card
